72314e551f76b6e1f7ab62f83a427154dfdca06b
Pierre Schaus <pschaus@gmail.com>
2022-09-18 17:06:33 +0200

    update

--- build_github.sh
+++ build_github.sh
@@ -19,49 +19,16 @@ make latexpdf
 #done
 #cd ../..
 
+
+f="linfo1121.pdf"
+
 # Upload Answer PDFs
 cd build/latex
-for f in s*.pdf
-do
-	curl -X POST https://content.dropboxapi.com/2/files/upload \
-    	--header "Authorization: Bearer $AUTHORIZATION" \
-    	--header "Dropbox-API-Arg: {\"path\": \"/tuteurs/answer-$f\", \"mode\":\"overwrite\"}" \
-    	--header "Content-Type: application/octet-stream" \
-    	--data-binary @$f
-done
-cd ../..
 
-rm -rf build
+curl -X POST https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization: Bearer $AUTHORIZATION" \
+    --header "Dropbox-API-Arg: {\"path\": \"/tuteurs/answer-$f\", \"mode\":\"overwrite\"}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @$f
 
-
-echo "show_answers = False" >> source/conf.py
-make latexpdf
-cd build/latex
-for f in s*.pdf
-do
-	curl -X POST https://content.dropboxapi.com/2/files/upload \
-    	--header "Authorization: Bearer $AUTHORIZATION" \
-    	--header "Dropbox-API-Arg: {\"path\": \"/tuteurs/$f\", \"mode\":\"overwrite\"}" \
-    	--header "Content-Type: application/octet-stream" \
-    	--data-binary @$f
-done
-cd ../..
-rm -rf build
-
-echo "respect_hide = True" >> source/conf.py
-echo "html_baseurl = 'https://pschaus.github.io/LINFO1121/'" >> source/conf.py
-make html
-make latexpdf
-cd build/latex
-for f in s*.pdf
-do
-	filesize=$(stat -c%s "$f" || gstat -c%s "$f")
-	if (( filesize > 20000 )); then
-		curl -X POST https://content.dropboxapi.com/2/files/upload \
-    		--header "Authorization: Bearer $AUTHORIZATION" \
-    		--header "Dropbox-API-Arg: {\"path\": \"/student/$f\", \"mode\":\"overwrite\"}" \
-    		--header "Content-Type: application/octet-stream" \
-    		--data-binary @$f
-    fi
-done
 cd ../..
