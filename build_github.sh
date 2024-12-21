947754be26f5c09322422a4d37ba830a9442722b
pschaus <pschaus@gmail.com>
2024-12-21 12:07:28 +0100

    update

--- build_github.sh
+++ build_github.sh
@@ -3,24 +3,24 @@ set -e
 
 rm -rf build
 
-echo "show_answers = True" >> source/conf.py
+#echo "show_answers = True" >> source/conf.py
 
-make latexpdf
+#make latexpdf
 
-f="linfo1121.pdf"
+#f="linfo1121.pdf"
 
 # Upload Answer PDFs
-cd build/latex
+#cd build/latex
 
 
 
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer $AUTHORIZATION" \
-    --header "Dropbox-API-Arg: {\"path\": \"/tuteurs/answer-$f\", \"mode\":\"overwrite\"}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @$f
+#curl -X POST https://content.dropboxapi.com/2/files/upload \
+#    --header "Authorization: Bearer $AUTHORIZATION" \
+#    --header "Dropbox-API-Arg: {\"path\": \"/tuteurs/answer-$f\", \"mode\":\"overwrite\"}" \
+#    --header "Content-Type: application/octet-stream" \
+#    --data-binary @$f
 
-cd ../..
+#cd ../..
 
 echo "show_answers = False" >> source/conf.py
 
